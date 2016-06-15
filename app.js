23d94475e3f273d412ad5e1d519e9da8c56528ce
kaylynyuh <yuhkay4@gmail.com>
2016-06-15 15:06:02 -0700

    6th question complete, 7th in prog

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var userName = prompt('Greetings! What is your name?');
+/* var userName = prompt('Greetings! What is your name?');
 console.log(userName + ' started the guessing game!');
 alert('Welcome ' + userName + ', glad you\'re here!');
 
@@ -47,4 +47,37 @@ if (answer5 === 'yes' || answer5 === 'y'){
 } else {
   console.log(userName + ' answered incorrectly!');
   alert('Wrong again my friend.');
+  */
+
+// var rightNumber = 22;
+// var i = 0;
+// while (i < 4) {
+//   var input = parseInt(prompt('Guess a number between 1 and 50... Don\'t be scared! You\'re probably wrong anyway.'));
+//   if (input === rightNumber){
+//     alert('Correct!');
+//     break;
+//   }
+//   else if (input < rightNumber){
+//     alert('Too Low!');
+//     i++;
+//   }
+//   else if (input > rightNumber){
+//     alert('Too High!');
+//  i++;
+//  }
+//}
+
+var correctAnswer = ['Washington', 'Oregon', 'California', 'Hawaii', 'Alaska', 'New York'];
+var counter = 0;
+while (counter < 6) {
+  for (var i = 0; i < correctAnswer.length; i++) {
+    var userInput = prompt('What are my favorite states?');
+    if (userInput === correctAnswer[i]) {
+      alert ('Way to be!');
+      counter = 7;
+    }
+    else {
+      alert ('Try again my friend');
+    }
+  }
 }
